ca2b39c49fd25e65da19dd63e85d9322c780f3dc
Anri Liy <amedamachip713@gmail.com>
2020-06-02 12:32:28 -0700

    Connect to database

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
+
+app.post("/todos", async(req, res) => {
+  try {
+    const {description} = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+    res.json(newTodo);
+  } catch(err) {
+    console.error(err.message);
+  }
+  
+})
+
 app.listen(5000, () => {
   console.log("server has been started on port 5000");
 });
