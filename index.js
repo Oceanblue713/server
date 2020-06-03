13f676d31f6fd126b7defe2aad36bb69f1cc405d
Anri Liy <amedamachip713@gmail.com>
2020-06-03 13:33:13 -0700

    Implement get request

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ app.post("/todos", async(req, res) => {
   try {
     const {description} = req.body;
     const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
   } catch(err) {
     console.error(err.message);
   }
-  
+})
+
+app.get("/todos", async(req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+})
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+
+    res.json(todo.rows[0])
+  } catch (err) {
+    console.log(err.message);
+  }
 })
 
 app.listen(5000, () => {
